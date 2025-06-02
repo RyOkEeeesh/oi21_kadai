16f076e6753fe16ffe64f1e76139026ca404ccea
RyOkEeeesh <dafe00842202@outlook.jp>
2025-06-02 15:05:45 +0900

    commit

--- io21kadai3/io21_kadai3_15_加地良寅.py
+++ io21kadai3/io21_kadai3_15_加地良寅.py
@@ -2,19 +2,17 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import RPi.GPIO as GPIO
 
-class status:
+class Status:
   def __init__(self):
     self.working = False
     self.status = None
     self.port = [13, 19, 26]
-  def	setstatus(self, s):
+  def	setStatus(self, s):
     self.status = s
-  def	getstatus(self):
-    return self.status
   def changeWorkStatus(self, bool):
     self.working = bool
 
-c = status()
+c = Status()
 app = Flask(__name__)
 CORS(app)
 GPIO.setmode(GPIO.BCM)
@@ -26,10 +24,10 @@ def echo():
   status = int(data.get('status', ''))
 
   if status == 4 and not(c.working):
-    c.setstatus(status)
+    c.setStatus(status)
   elif status != c.status and status >= 0 and status < 4:
     GPIO.output(c.port, GPIO.LOW)
-    c.setstatus(status)
+    c.setStatus(status)
     c.changeWorkStatus(True)
     if c.status != 3: GPIO.output(c.port[c.status], GPIO.HIGH)
 
